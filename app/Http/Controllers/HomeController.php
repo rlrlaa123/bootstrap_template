b071c6862f3480af147ee6b2022674634b63db00
bosung2697 <36359088+bosung2697@users.noreply.github.com>
2018-03-12 10:57:53 +0900

    CodVice Home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,8 +9,6 @@ class HomeController extends Controller
 {
     public function index(){
 
-        $intro = Intro::all();
-
-        return view('home',compact('intro'));
+        return view('home');
     }
 }
